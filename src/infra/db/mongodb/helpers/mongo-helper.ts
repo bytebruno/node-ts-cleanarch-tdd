9e7d8eab0cd9bdf1de217b73234773c745680185
Bruno Rodrigues <bytebruno@gmail.com>
2021-11-08 21:51:33 -0300

    refactor: add mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb'
+
+const MONGO_URL = process.env.MONGO_URL ?? ''
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    const options = { }
+    this.client = await MongoClient.connect(MONGO_URL, options)
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  }
+}
